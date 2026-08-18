006240033c83a2f9fc403690536fd4a9fa19ace1
Houssem <houssemnaffouti28@gmail.com>
2026-08-18 21:44:28 +0100

    ttt

--- providers.tf
+++ providers.tf
@@ -2,19 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
-  }
-
-  backend "s3" {
-    bucket         = "terraform-state-houssem-tn-84213"
-    key            = "terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
   }
 }
 
 provider "aws" {
-  region = "eu-west-3"
+  region = var.aws_region
 }
